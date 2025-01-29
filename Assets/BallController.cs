b9433f9f768802ba0bf3a81dfa383b40ef69e317
Frank Wang <frankwang0098@gmail.com>
2025-01-28 21:36:32 -0800

    added WASD movement to ball

---
+++ Assets/BallController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public Rigidbody sphereRigidbody;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 inputVector = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector += Vector2.up;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector += Vector2.down;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector += Vector2.left;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector += Vector2.right;
+        }
+
+        Vector3 inputXZPlane = new Vector3(inputVector.x, 0, inputVector.y);
+        sphereRigidbody.AddForce(inputXZPlane);
+
+    }
+}
